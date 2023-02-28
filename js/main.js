4a9c5362b60e07fc17b73f571620b09f4e8d5e36
js choi <photosynthesis0131@gmail.com>
2023-02-28 17:49:24 +0900

    2023-02-28 add goTop button

--- js/main.js
+++ js/main.js
@@ -7,20 +7,24 @@ $(document).ready(function() {
 		anchors:['page1', 'page2', 'page3', 'page4', 'page5','page6'],
 		afterLoad: function(anchorLink, index){            
       var slice = anchorLink.slice(4);      
+      $('.go-top').click(function(){
+        $.fn.fullpage.moveTo(1);     
+      })
       if(index == 1){      
 				$('.section.step1').find('h2').addClass('visible');			
 				$('.section.step1').find('p').addClass('visible');			
       }; 
       if(index == slice){
-        var classNum = ($('.gnb_background .eng').find('a').eq(index - 1));        
-        console.log(classNum)
+        var classNum = ($('.gnb_background .eng').find('a').eq(index - 1));                
         $('.gnb_background .eng').find('a').removeClass('add-color');
         classNum.addClass('add-color');        
       }            
 			if(index == 6){                
+        $('.section.last_step').addClass('visible');
 				$('.section.last_step').find('h2').addClass('fade_down');
 				$('.section.last_step').find('.contact').addClass('fade_up');
       }else{				
+        $('.section.last_step').removeClass('visible');
 				$('.section.last_step').find('h2').removeClass('fade_down');
 				$('.section.last_step').find('.contact').removeClass('fade_up');
       };		
